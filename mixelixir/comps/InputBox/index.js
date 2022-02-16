04e67e2ae9e97432c691b6ea2763a8c2e3cfa487
Abhay <abakshi4@my.bcit.ca>
2022-02-15 18:47:32 -0800

    initializing components

---
+++ mixelixir/comps/InputBox/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// styled component imports
+import { 
+    InputCont,
+    InputField 
+} from './styles'
+
+export const Input = () => {
+return (
+    <InputCont>
+        <InputField  placeholder='Search Ingredients here' />
+    </InputCont>
+)
+}
---
+++ mixelixir/comps/InputBox/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+
+export const InputCont = styled.div`
+
+`
+
+export const InputField = styled.input`
+width:300px;
+height:100px;
+`
