5f9aeed77047ffa18bfa950a9e3e71ca46bb2425
Abhay <abakshi4@my.bcit.ca>
2022-02-20 15:07:23 -0800

    configuring generator and connecting to the local api

--- mixelixir/pages/api/drinks.js
+++ mixelixir/pages/api/drinks.js
@@ -2,19 +2,16 @@ import {generate} from '../../utils/func'
 import all_drinks from '@/all_drinks';
 
 
-export default async function handler(req, res) {
+export default async function handler(req, res) 
+{
 
-  //HELPER FUNCTIONS FOR YOU TO USE!
-  console.log(req.query)
-  //await Save("test", json);
-  //const files = await Read();
-var cocktails =[]
-  //detect if filter/save/read
-  const ings = req.query;
+  var cocktailMatches =[]
+  const receivedParams = req.query;
+  const comparitiveArray = Object.values(receivedParams)
 
-  if (ings){
- cocktails = generate(all_drinks, ings)
-    console.log("whooooooooo", cocktails)
+  if (comparitiveArray)
+  {
+    cocktailMatches = generate(all_drinks, comparitiveArray)
   }
-  res.status(200).json(cocktails);
+    res.status(200).json(cocktailMatches);
 }
--- mixelixir/utils/func.js
+++ mixelixir/utils/func.js
@@ -1,72 +1,78 @@
+const all_drinks = require('../all_drinks.js')
 const drinks = require('../all_drinks.js')
 
 var ing = ['orange peel', 'whiskey', 'lemon', 'gin']
 
 
-export function generate (data,ing)
+ export function generate (data,ing)
 {
     try{
-    var sorted =[]
-    data.map(obj =>{
-    let temp =[]
-    let temp_two =[]
-    let ingredients = []
-    for (var i=1; i<16;i++)
-    {
-        temp.push(obj["strIngredient"+i])
-        // temp_two.push(Object.entries(obj).includes('strMeasure'+ i))
-    }
-
-    temp.map((o,i)=>{
-        if(o != '')
-        {ingredients.push(o)}
-    })
-
-    temp_two.push(Object.fromEntries(Object.entries(obj).filter(([key]) => !key.includes('strIngredient'))));
-
-    temp_two.map((o,i)=> Object.assign(o,{ingredients:ingredients}))
-    console.log(temp_two)
-    if(ing)
-    {
-        const filtered = temp_two.map((o)=>
+        var drinkMatches =[]
+        data.map(obj =>
         {
-            // console.log(o)
-            var count = 0
-                        o.ingredients.filter((el)=>
-                        { 
-                            var work = false
-                            for(var i = 0; i<ing.length; i++)
-                            {
-                                if (el.toLowerCase().includes(ing[i].toLowerCase()))
-                                {
-                                    count ++
-                                    // else if (count < 2)
-                                    // {
-                                    //     // el.splice(0,i)
-                                    //     // sorted.splice(i,1)
-                                    //     return false
-                                    // }
-                                }
+                
+                let storedIngredients =[]// array to extract and store all the ingredients from the data in an array format
+                let ingredients = [] // array to store all extracted ingredients by after filtering out the empty values
+                let formatedDrinks =[] // array to store the data in a formated manner i.e consolidating seperate ingredient key value pairs into one singular array of key : [array of ingredients for each drink in the data]
+                
 
-                            }
-                            //console.log(el, count, ing)
+                // reformating data into the aforementioned format
+                for (var i=1; i<16;i++)
+                {
+                    storedIngredients.push(obj["strIngredient"+i])
+                }
 
-                            if(count >= 2)
-                            {
-                                sorted.push(o);
-                            }
-                            return work;
-
-                        })
-                            
-
-        })
-                        
-                        //console.log( filtered)
-    }
+                storedIngredients.map((o,i)=>
+                {
+                    if(o != '')
+                    {ingredients.push(o)}
                 })
-                console.log(sorted)
-                return sorted.length === 0 ? null : sorted
-            }
-            catch(err){ console.log(err), 'not workingg'}
+
+                // constructing a new object with the everything but the ingredients 
+                formatedDrinks.push(Object.fromEntries(Object.entries(obj).filter(([key]) => !key.includes('strIngredient'))));
+                // pushing the formatted ingredients into the newly constructed object
+                formatedDrinks.map((o,i)=> Object.assign(o,{ingredients:ingredients}))
+                
+                // if a comparitive array exists i.e. the second parameter of the function which will be a user inputted array of ingredients from a textbox on front end
+                if(ing)
+                {   
+                    // using filter as a loop to go over all the data
+                    const filtered = formatedDrinks.map((o)=>
+                    {   
+                        var count = 0 //keeping track of how many items inside the comparitive array matches with the dataset's ingredient arrays
+                                    o.ingredients.filter((el)=>
+                                    { 
+                                        var work = false
+                                        for(var i = 0; i<ing.length; i++)
+                                        {
+                                            // comparing if dataset's contains mutual ingredients with the comparitive array regardless of the case senstivity on both ends
+                                            if (el.toLowerCase().includes(ing[i].toLowerCase()))
+                                            {
+                                                count ++//increases the count by one everytime there's a match.
+                                                
+                                            }
+
+                                        }
+
+                                        // condition to check if there's at least 2 or more matches
+                                        if(count >= 2 && !drinkMatches.includes(o))
+                                        {
+                                            drinkMatches.push(o);//push the drink object that meets the condition
+                                        }
+                                        return work;
+
+                                    })
+                                        
+
+                    })
+                        
+
+                }
+        })
+                // returning all not null drinking matches and filtering out the empty arrays
+                return drinkMatches.length === 0 ? null : drinkMatches
+    }
+    catch(err){ console.log(err), 'not workingg'}
 }
+
+// generate(all_drinks,ing)
