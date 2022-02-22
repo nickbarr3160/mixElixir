2413e67085421d8474033d3534050da595e48d6f
Abhay <abakshi4@my.bcit.ca>
2022-02-22 14:53:16 -0800

    comitting before mrging with nick's latest changes

--- mixelixir/comps/IngredientsList/index.js
+++ mixelixir/comps/IngredientsList/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import { ListWrapper } from './styles'
+import GrClose from 'react-icons/gr'
 
+import { 
+  ListWrapper, 
+  ListItem,
+  IngName,
+  DeleteIcon
+} from './styles'
 
+// const defaultData = [
+//   '0','1','2','3'
+// ]
 
-export const IngredientList = ({items}) => {
+export const IngredientList = ({
+  items, 
+  ingredients =[]
+}) => {
+
   return (
     <ListWrapper>
       {items}
--- mixelixir/comps/IngredientsList/styles.js
+++ mixelixir/comps/IngredientsList/styles.js
@@ -5,3 +5,12 @@ height:30%;
 width:50%;
 border:2px solid red;
 `
+export const ListItem = styled.div`
+display:flex;
+width:100%;
+height:50px;
+border:1px solid aqua;
+`
+export const IngName = styled.span``
+
+export const DeleteIcon = styled.div
